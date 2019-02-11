d5e1a16b2602355107b6e7bfa965605b4678ef1f
DaniCelis <36241903+DaniCelis@users.noreply.github.com>
2019-02-11 16:18:21 -0500

    Update base.pl

--- base.pl
+++ base.pl
@@ -4,14 +4,23 @@ padreDe(pablo,marcela).
 padreDe(carlos,debora).
 padreDe(luisa,debora).
 padreDe(debora,han).
+padreDe(sam,lola).
+padreDe(ana,lola).
+padreDe(sam,juana).
+padreDe(ana,juana).
+padreDe(lola,pati).
+padreDe(john,pati).
+
 
 hijoDe(A,B):-padreDe(B,A).
 
 abuelode(A,B) :- padreDe(A,C), padreDe(C,B).
 
 hermanode(A,B) :- padreDe(C,A), padreDe(C,B), A \== B.
+
 escasado(A,B) :- padreDe(A,C), padreDe(B,C), A \== B.
-esfeliz(A,B):- notescasado(A,B).
+
+esfeliz(A):- not(escasado(A,_)).
 
 familiarde(A,B) :- padreDe(A,B).
 familiarde(A,B) :- abuelode(A,B).
